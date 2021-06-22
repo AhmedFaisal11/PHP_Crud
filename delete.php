848fc315d685c67b927c40ebf5b84dd8e225c903
Ahmedbinfaisal <67901435+AhmedFaisal11@users.noreply.github.com>
2021-06-22 09:10:16 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,11 +2,15 @@
 
 include 'conn.php';
 
+// extracting the user data using get method 
 extract($_GET);
+
+// making query for deleting data in mysql
 $query = "DELETE FROM user WHERE id='$id'";
 
+// run query 
 mysqli_query($conn,$query);
 
 header('location:display.php');
-
+// this header will redirect back to display.php page
 ?>
